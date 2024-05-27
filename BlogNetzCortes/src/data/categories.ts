b8f154f6d12df5fe8c8069fa1f1cad110f5c1730
hector cortes <hcortes@mlti.com.mx>
2024-05-27 00:09:40 -0600

    configure init site

--- BlogNetzCortes/src/data/categories.ts
+++ BlogNetzCortes/src/data/categories.ts
@@ -1,8 +1,8 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+	'frontend Development',
+	'backend development',
+	'web design',
+	'database management',
+	'tips for devs'
 ] as const
--- BlogNetzCortes/src/data/site.config.ts
+++ BlogNetzCortes/src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
+	author: 'migcortes', // Site author
+	title: 'Blog of netzcortes', // Site title.
+	description: 'this app development for share content of TI resources', // Description to display in the meta tags
+	lang: 'es',
+	ogLocale: 'es',
+	shareMessage: 'netzcortes', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
