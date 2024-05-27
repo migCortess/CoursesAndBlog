28679ff1a749209865cdbc500ba028cb188a69b4
hector cortes <hcortes@mlti.com.mx>
2024-05-26 23:03:20 -0600

    add template and CMS

--- BlogNetzCortes/astro.config.mjs
+++ BlogNetzCortes/astro.config.mjs
@@ -1,4 +1,32 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { remarkReadingTime } from './src/utils/readTime.ts'
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://netzcortes.com/', // Write here your website url
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		drafts: true,
+		shikiConfig: {
+			theme: 'material-theme-palenight',
+			wrap: true
+		}
+	},
+  integrations: [react(),mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      experimentalThemes: {
+        light: 'vitesse-light',
+        dark: 'material-theme-palenight',
+        },
+      wrap: true
+    },
+    drafts: true
+  }),
+  sitemap(),
+  tailwind()]
+});
